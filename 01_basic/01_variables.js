33022a6e53d7463f25e659694abc30b17f4e1ba6
QAZI AHMED MUDDASSIR <83705875+qaziamuddassir@users.noreply.github.com>
2025-01-19 21:33:18 +0000

    commit variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 9806
+let accounrName = "12"
+var accountEmai = "Qazi@qazi.com"
+accountCity = "kolkat"
+// accountId = 21
+let hello;
+var hi;
+console.table([accountId, accounrName, accountEmai, accountCity, hello, hi])
