4dffa74cb6f8fac3b82a3e5b9866009fc24a8e0d
QAZI AHMED MUDDASSIR <83705875+qaziamuddassir@users.noreply.github.com>
2025-01-28 18:59:29 +0000

    num

---
+++ 01_basic/num_maths.js
@@ -0,0 +1,17 @@
+const score = 400
+let num = 1250.25821
+const balance = new Number(12560)
+
+// console.log(score);
+// console.log(balance);
+// let ping = (balance.toString());
+// console.log(typeof(ping));
+// console.log(ping);
+// console.log(num.toFixed(2))
+// let name = 123564862
+// console.log(num.toPrecision(4))
+// console.log(name.toLocaleString('en-IN'));
+
+// +++++++++++++++ Maths ++++++++++++++++++++
+
+
