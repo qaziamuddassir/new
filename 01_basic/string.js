99670b61f70523c7553b5af726f399fa2ae95b5f
QAZI AHMED MUDDASSIR <83705875+qaziamuddassir@users.noreply.github.com>
2025-01-28 15:46:23 +0000

    string

---
+++ 01_basic/string.js
@@ -0,0 +1,8 @@
+const name = "Muddassir";
+
+const gameName = new String("Nobel-Gorilla15");
+
+// console.log(name + gameName)
+// console.log(`My name is ${name} and my game name is ${gameName.toUpperCase()}`)
+// console.log(name.indexOf("s"));
+// console.log(name.charAt("6"));
